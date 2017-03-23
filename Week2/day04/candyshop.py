529be0971d6415a3414aeabef470a1bdcc2c55d7
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-03-23 16:37:59 +0100

    found solution for element.py

---
+++ Week2/day04/candyshop.py
@@ -0,0 +1,9 @@
+shop_items = ["Cupcake", 2, "Brownie", false]
+
+# Accidentally we added "2" and "false" to the list.
+# Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+# No, don't just remove the items :)
+
+shop_items[-1] = "Ice Cream"
+shop_items[1] = "Croissant"
+print(shop_items)
