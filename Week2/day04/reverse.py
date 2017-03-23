3f499ca9037013ae0cc3caec399ff545bdce15d1
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-03-23 12:31:55 +0100

    solutions changed, replacing string methods with slicing

---
+++ Week2/day04/reverse.py
@@ -0,0 +1,4 @@
+# Reverse the string using StringBuilder
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+reversed[::-1]
--- Week2/day04/simplereplace.py
+++ Week2/day04/simplereplace.py
@@ -3,5 +3,5 @@ example = "In a dishwasher far far away";
 # I would like to replace "dishwasher" with "galaxy" in this example
 # Please fix it for me!
 # Expected ouput: In a galaxy far far away
-example = example.replace("dishwasher", "galaxy")
-print(example)
+
+print(example[:5] + 'galaxy ' + example[16:])
