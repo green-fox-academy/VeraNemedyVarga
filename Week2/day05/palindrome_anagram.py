1506f23e8b64ac7dfcb31161c6d9827ad324e07c
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-03-26 21:22:04 +0200

    palindrome.py fixed

--- Week2/day05/palindrome_anagram.py
+++ Week2/day05/palindrome_anagram.py
@@ -1,27 +1,25 @@
-#Build palindrome from word
 def palindrome_builder(word):
     print( word + word[::-1])
 palindrome_builder('vera')
 
-#Check if word is palindrome
 def is_palindrome(word):
-    return word == word[::-1] #check if reverse is the same as original
+    return word == word[::-1]
 
-
-#text example
 sentence = "dog goat dad duck doodle never"
-palindromes = []
 
-#Palindromes in sentence finder
-for j in range(0, len(sentence)):   #iterate over first index of sentence slice
-    for i in range(3, len(sentence)):   #iterate over second index
-        if is_palindrome(sentence[j:(i + 1)]):  #call is_palindrome on slice
-            if 3 <= len(sentence[j:(i + 1)]):   #only selects the ones with length 3 or over
-                palindromes.append(sentence[j:(i + 1)])     #adds it to a list
 
-print(palindromes)
+def search_palindrome(sentence):
+    palindromes = []
+    for j in range(0, len(sentence)):
+        for i in range(3, len(sentence)):
+            if is_palindrome(sentence[j:(i + 1)]):
+                if 3 <= len(sentence[j:(i + 1)]):
+                    palindromes.append(sentence[j:(i + 1)])
+    print(palindromes)
 
-#Anagram
+search_palindrome(sentence)
+
+
 def anagram(word1, word2):
     #str to boolean
     for letter in word1:
@@ -30,5 +28,5 @@ def anagram(word1, word2):
         else:
             return False
 
-print(anagram("dog", "baba"))       #test, should print False
-print(anagram("god", "dog"))        #test, should print True
+print(anagram("dog", "baba"))
+print(anagram("god", "dog"))
