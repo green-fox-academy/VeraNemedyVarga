23185321d54672f62add46f1ee71a7f0caaa9465
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-03-28 17:11:28 +0200

    4encoded with solution added

--- Week3/day02/exercise04/crypto_4encoded.py
+++ Week3/day02/exercise04/crypto_4encoded.py
@@ -3,14 +3,40 @@ def decrypt(file_name):
     from builtins import ascii
     content = open(file_name, 'r')
     read_content = content.read()
-    new_text = ""
-    for char in (0, len(read_content)):
-        char += 1
-        new_text += read_content[char]
-    print(new_text)
 
-decrypt(encoded_lines.txt)
+    decrypted = ""
+    for letter in read_content:
+        if letter == " ":
+            decrypted += " "
+        elif letter == '\n':
+            decrypted += "\n"
+        elif letter == "\t":
+            decrypted += ""
+        else:
+            numify = ord(letter)
+            numify -= 1
+            textify = chr(numify)
+            decrypted += textify
 
+    wfile = open("solution.txt", 'w')
+    wfile.write(decrypted)
+    wfile.close()
+    content.close()
+
+    return decrypted
+
+print(decrypt("encoded_lines.txt"))
+
+"""encoded = "Cfbvujgvm jt cfuufs uibo vhmz/ Fyqmjdju jt cfuufs uibo jnqmjdju/"
+decrypted = ""
+for letter in encoded:
+    if letter == " ":
+        decrypted += " "
+    numify = ord(letter)
+    numify -= 1
+    textify = chr(numify)
+    decrypted += textify
+print(decrypted)"""
 
 
 #char index asciiban i = i + 1 legyen.
