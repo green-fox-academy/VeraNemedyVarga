10b09f8c915d624f01105044d66e016c241ce423
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-03-29 17:51:55 +0200

    actual purple steps added, 3d name corrected

--- Week3/day03/purple_steps.py
+++ Week3/day03/purple_steps.py
@@ -7,10 +7,11 @@ canvas.pack()
 
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/purple-steps/r3.png]
-x = 10
+x = 20
 y = 20
-for rect in range(6):
-    rect = canvas.create_rectangle(x, x, y, y, fill= "purple")
-    x = y
-    y *= 1.5
+
+for rect in range(20):
+    canvas.create_rectangle(x, x, x+10, x+10, fill= "purple")
+    x +=10
+
 root.mainloop()
