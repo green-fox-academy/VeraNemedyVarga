4e3b7b58797f0da89442340c1c07d188af35aedd
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-03-31 14:31:52 +0200

    square grid solved, with feat of diiferent filling colors

---
+++ Week3/day05/square_grid.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+root = Tk()
+canvas = Canvas(root, width='600', height='600')
+canvas.pack()
+
+colors = ["red", "pink", "yellow", "blue", "black", "purple", "silver", "pink", "green"]
+def draw_frame(x, y, size, yoyo):
+    canvas.create_rectangle(x-size/2, y-size/2, x+size/2, y+size/2, fill=colors[int(yoyo/5)], width = yoyo, outline="red")
+
+def recursive_frame(x, y, size, yoyo):
+    draw_frame(x, y, size, yoyo)
+    if yoyo > 5:
+        recursive_frame(x-size/2, y-size/2, size/2, yoyo/2)
+        recursive_frame(x-size/2, y+size/2, size/2, yoyo/2)
+        recursive_frame(x+size/2, y+size/2, size/2, yoyo/2)
+        recursive_frame(x+size/2, y-size/2, size/2, yoyo/2)
+
+
+recursive_frame(300, 300, 300, 40)
+
+root.mainloop()
