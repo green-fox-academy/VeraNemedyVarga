8b3d1d0f611a8950d7eb4600e2f322051114a96e
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-04 11:44:03 +0200

    Mentor and Sponsor classes added, working

--- Week4/day02/green_fox_organization.py
+++ Week4/day02/green_fox_organization.py
@@ -22,10 +22,52 @@ class Student(Person):
     def introduce(self):
         print("Hi, I'm " + self.name + " a " + str(self.age) + " year old " + self.gender + " from " + self.prev_org + " who skipped " + str(self.skipped_days) + " from the course already.")
 
+    def skip_days(self, number_of_days):
+        self.number_of_days = number_of_days
+        self.skipped_days += self.number_of_days
+        return self.skipped_days
+
+
+class Mentor(Person):
+    def __init__(self, name='Jane Doe', age=30, gender='female', level= "Intermediate"):
+        super().__init__(name, age, gender)
+        self.level = level
+
+    def introduce(self):
+        print("Hi, I'm " + self.name + " a " + str(self.age) + " year old " + self.gender + " " + self.level + " mentor.")
+
+class Sponsor(Person):
+    def __init__(self, name='Jane Doe', age=30, gender='female', company="Google", hired_students = 0):
+        super().__init__(name, age, gender)
+        self.company = company
+        self.hired_students = hired_students
+
+    def introduce(self):
+        print("Hi, I'm " + self.name + " a " + str(self.age) + " year old " + self.gender + " who represents " + self.company + " and hired " + str(self.hired_students) + " students so far.")
+
+    def hire(self):
+        self.hired_students += 1
+        return self.hired_students
+
+    def get_goal(self):
+        print("Hire brilliant junioor software developers.")
+
+class LagopusClass(object):
+    
 
 neni = Person()
 neni.introduce()
 neni.get_goal()
+
 student = Student()
 student.get_goal()
 student.introduce()
+print(student.skip_days(4))
+
+mentor = Mentor()
+mentor.introduce()
+
+sponsor = Sponsor()
+sponsor.introduce()
+print(sponsor.hire())
+sponsor.get_goal()
