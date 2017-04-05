e50b89d9b3ce39e36e1c015f0d8e4d0518cf32a7
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-05 13:31:41 +0200

    Sum class with sumall method added, passed unit testing

--- Week4/day03/vera_nemedyvarga_test.py
+++ Week4/day03/vera_nemedyvarga_test.py
@@ -6,6 +6,26 @@ class TestApple(unittest.TestCase):
         apple = Apple()
         self.assertEqual(apple.get_apple(), "apple")
 
+class TestSum(unittest.TestCase):
+    def test_sumall(self, integers = []):
+        summa = Sum()
+        integers = [3, 4, 5]
+        self.assertEqual(summa.sumall(integers), 12)
+
+    def test_sumall_empty_list(self, integers = []):
+        summa = Sum()
+        integers = []
+        self.assertEqual(summa.sumall(integers), 0)
+
+    def test_sumall_one_element_list(self, integers = []):
+        summa = Sum()
+        integers = [8]
+        self.assertEqual(summa.sumall(integers), 8)
+
+    def test_sumall_zero_inlist(self, integers = []):
+        summa = Sum()
+        integers = [0]
+        self.assertEqual(summa.sumall(integers), 0)
 
 if __name__ == '__main__':
     unittest.main()
--- Week4/day03/vera_nemedyvarga_work.py
+++ Week4/day03/vera_nemedyvarga_work.py
@@ -1,3 +1,8 @@
 class Apple(object):
     def get_apple(self):
         return "apple"
+
+class Sum():
+    def sumall(self, integers = []):
+        self.integers = integers
+        return sum(self.integers)
