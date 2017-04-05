8c8190f089aedab7b8cc59093269df9057ac68db
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-05 14:01:22 +0200

    anagram class and method added, passed unit testing

--- Week4/day03/vera_nemedyvarga_test.py
+++ Week4/day03/vera_nemedyvarga_test.py
@@ -7,25 +7,26 @@ class TestApple(unittest.TestCase):
         self.assertEqual(apple.get_apple(), "apple")
 
 class TestSum(unittest.TestCase):
-    def test_sumall(self, integers = []):
+    def test_sumall(self):
         summa = Sum()
-        integers = [3, 4, 5]
-        self.assertEqual(summa.sumall(integers), 12)
+        self.assertEqual(summa.sumall([3, 4, 5]), 12)
 
-    def test_sumall_empty_list(self, integers = []):
+    def test_sumall_empty_list(self):
         summa = Sum()
-        integers = []
-        self.assertEqual(summa.sumall(integers), 0)
+        self.assertEqual(summa.sumall([]), 0)
 
-    def test_sumall_one_element_list(self, integers = []):
+    def test_sumall_one_element_list(self):
         summa = Sum()
-        integers = [8]
-        self.assertEqual(summa.sumall(integers), 8)
+        self.assertEqual(summa.sumall([8]), 8)
 
-    def test_sumall_zero_inlist(self, integers = []):
+    def test_sumall_zero_inlist(self):
         summa = Sum()
-        integers = [0]
-        self.assertEqual(summa.sumall(integers), 0)
+        self.assertEqual(summa.sumall([0]), 0)
+
+class TestAnagram(unittest.TestCase):
+    def test_anagram(self):
+        ana = Anagram()
+        self.assertEqual(ana.anagram("", ""), True)
 
 if __name__ == '__main__':
     unittest.main()
--- Week4/day03/vera_nemedyvarga_work.py
+++ Week4/day03/vera_nemedyvarga_work.py
@@ -6,3 +6,15 @@ class Sum():
     def sumall(self, integers = []):
         self.integers = integers
         return sum(self.integers)
+
+class Anagram():
+    def anagram(self, word1, word2):
+        self.word1 = word1
+        self.word2 = word2
+        if ''.join(sorted(self.word1))==''.join(sorted(self.word2)):
+            return True
+        else:
+            return False
+
+# Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+# Create a test for that.
