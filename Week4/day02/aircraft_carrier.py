757b86c5aa380fb411a9cfe785fef55269b1bd45
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-15 21:01:53 +0200

    Carrier class created w constructor, add_aircraft method works like a boss

--- Week4/day02/aircraft_carrier.py
+++ Week4/day02/aircraft_carrier.py
@@ -25,22 +25,36 @@ class Aircraft():
     def get_status(self):
         return str(self.aircraft_type) + " " + str(self.current_ammo) + " " + str(self.base_damage) + " " + str(self.damage)
 
-
 class F16(Aircraft):
     def __init__(self):
         super().__init__(max_ammo = 8, base_damage = 30, aircraft_type = "F16")
-
-
 
 class F35(Aircraft):
     def __init__(self):
         super().__init__(max_ammo = 12, base_damage = 50, aircraft_type = "F35")
 
+class Carrier():
+    def __init__(self, current_carrier_ammo, health_point):
+        self.current_carrir_ammo = current_carrier_ammo
+        self.aircraft_list = []
+        self.health_point = health_point
+
+    def add_aircraft(self, aircraft_type):
+        if aircraft_type == "f16":
+            self.aircraft_list.append(F16())
+        elif aircraft_type == "f35":
+            self.aircraft_list.append(F35())
+
 
 f16 = F16()
 f35 =  F35()
+carrier = Carrier(0, 0)
 print(f16.base_damage)
 print(f35.max_ammo)
 print(f16.get_type())
 print(f16.refill(10))
 print(f16.get_status())
+carrier.add_aircraft("f16")
+carrier.add_aircraft("f35")
+print(carrier.aircraft_list[0].get_type())
+print(carrier.aircraft_list[1].get_type())
