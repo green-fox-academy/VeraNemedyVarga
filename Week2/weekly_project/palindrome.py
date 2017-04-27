d9e3633e62771badaf0b14264e70210e36d60d62
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-27 17:21:57 +0200

    anagram checker function finally added

--- Week2/weekly_project/palindrome.py
+++ Week2/weekly_project/palindrome.py
@@ -1,35 +1,23 @@
-"""a = "adika"
+def is_anagram(word1, word2):
+	if ''.join(sorted(word1)) == ''.join(sorted(word2)):
+		print(True)
+	else:
+		print(False)
 
-for letter in range(len(a)-1, -1, -1):
-	print(a[letter])"""
-
-# def create_palindrome(word):
-#     return word + word[::-1]
-#
-# print(create_palindrome('malac'))
-#
 def is_palindrome(word):
     return word == word[::-1]
 
 def search_palindrome(sentence):
     palindromes = []
-
     subsets = words_w_nchars(sentence)
 
     for word in subsets:
         if is_palindrome(word):
             palindromes.append(word)
-
     return palindromes
 
 sentence = 'dog goat dad duck doodle never'
 
-#
-# for letter in sentence:
-#     if len(word) >= 3:
-
-# str, num -> list
-# asda -> [asd, asda, sda]
 def words_w_nchars(word, n = 3):
     words = []
     length = len(word)
@@ -37,7 +25,7 @@ def words_w_nchars(word, n = 3):
         for l in range(n, length):
             if (l + index) <= length:
                 words.append(word[index : (index + l)])
-
     return words
 
 print(search_palindrome(sentence))
+is_anagram("dog", "god")
