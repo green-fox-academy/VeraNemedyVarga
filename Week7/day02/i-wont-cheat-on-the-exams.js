13a0504850076342b981e5658093192fd943ac17
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-02 15:26:38 +0200

    prints out string a 100 times

---
+++ Week7/day02/i-wont-cheat-on-the-exams.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+var notGonnaCheat = "I won't cheat on the exam! \n";
+console.log(notGonnaCheat.repeat(100));
