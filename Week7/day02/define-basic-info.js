2f2eb1f7ecb462ae3f5d779cc924500cfcad4f71
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-02 14:05:57 +0200

    prints out values of basic info variables

---
+++ Week7/day02/define-basic-info.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var myName = "Vera";
+var myAge = 26;
+var myHeight = 1.58;
+var isMarried = false;
+
+console.log(myName + " " + myAge + " " + myHeight + " " + isMarried)
