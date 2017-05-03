79988ee1d20d0ef41ba56a4cc29e0cce11b04b95
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-03 15:35:07 +0200

    sums elements in array

---
+++ Week7/day03/sum-all.js
@@ -0,0 +1,6 @@
+'use strict';
+// - Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// - Log the sum of the elements in `ai` to the console
+
+var ai = [2, 3, 5, 6, 7];
+console.log(ai.reduce((a, b) => a + b, 0));
