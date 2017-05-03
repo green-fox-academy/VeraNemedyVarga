ac1ce2cd4ee7c20ee35c607a4541ded57f939e7c
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-03 12:59:15 +0200

    sums numbers up to given parameter

---
+++ Week7/day03/sum.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Write a function called `sum` that sum all the numbers until the given parameter
+// - The function should return the result
+
+function sum(endNum) {
+    var result = 0;
+    for (var i = 1; i < endNum + 1; i++) {
+        result += i;
+    }
+    return result
+}
+
+console.log(sum(4));
