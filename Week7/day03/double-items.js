d463b6132cbcff9df7d8ea2114e33796332b7a96
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-03 15:24:48 +0200

    double items in array

---
+++ Week7/day03/double-items.js
@@ -0,0 +1,10 @@
+'use strict';
+// - Create an array variable named `ag` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// - Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+var ag = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+for (var item = 0; item < ag.length; item++) {
+    ag[item] = ag[item].repeat(2);
+}
+console.log(ag);
