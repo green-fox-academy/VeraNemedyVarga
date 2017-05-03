b8c59f123231e9a7e42ea427420d270ff942412f
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-03 11:41:43 +0200

    greets input variable with exception case handling

---
+++ Week7/day03/greet.js
@@ -0,0 +1,18 @@
+'use strict';
+// - Create variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings, dear Greenfox`
+//     - Prepare for the special case when no parameters are given
+// - Greet `al`
+
+var al = "Greenfox"
+
+function greet(name) {
+    if (typeof name === 'string') {
+        console.log("Greetings, dear", name)
+    } else {
+        console.log("No one to greet");
+    }
+}
+
+greet(al)
