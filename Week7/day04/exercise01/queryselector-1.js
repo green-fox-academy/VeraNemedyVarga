0ef5858e488b5cc166d4d37cf35f42e0661ede2a
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-04 11:50:04 +0200

    exercise complete with external js file

---
+++ Week7/day04/exercise01/queryselector-1.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var king = document.getElementById('b325');
+console.log(king);
+
+var conceitedMan = document.querySelector('.b326');
+alert(conceitedMan);
+
+var businessLamp = document.querySelectorAll('.big');
+console.log(businessLamp[0], businessLamp[1]);
+
+var conceitedKing = document.querySelectorAll('section div');
+alert(conceitedKing);
+
+var noBusiness = document.querySelectorAll('div');
+console.log(noBusiness);
+
+var allBizniss = document.querySelector('p');
+alert(allBizniss);
