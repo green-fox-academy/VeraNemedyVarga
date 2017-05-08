2741ae26013eee2cfaf5643b807e839e04241ccd
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-08 12:27:11 +0200

    refactored to one validator function, more than one occurence of list element checked

--- Week8/day01/index.js
+++ Week8/day01/index.js
@@ -8,27 +8,31 @@ var test = require('tape');
 // [2,1,3,4,5,6,7,8,9] => ok
 // [9,8,7,6,5,1,2,4,9] => Should fail
 // [9,8,7,6,5,1,2,4,'u'] => Shold fail (non integer)
-var completeList = [1,2,3,5,5,6,7,8,9];
-var differentOrder = [9,8,7,6,5,4,3,2,1];
+var completeList = [1, 2, 3, 5, 5, 6, 7, 8, 9];
+var differentOrder = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+var stringList = ['w', 2, 3, 4, 5, 6, 7, 8, 9];
+var incompleteList = [1, 2, 3, 4, 5];
+var multipleOccurance = [1, 2, 1, 3, 4, 5, 6, 7, 8];
 
-
-function lengthValidator (input) {
-    if (completeList.length === 9) {
+function validator (input) {
+    for (var index = 0; index < input.length; index++) {
+        if (input[index].typeOf !== 'String' && input.length === 9 && input.sort()[index] !== input.sort()[index+1]) {
         return true;
+        }
     }
 };
 
-function ignoreOrdervalidator(input) {
-    if (completeList === differentOrder);
-    return true;
-}
-
 test('Tests if there are nine numbers in list', function (t) {
-    t.equal(lengthValidator(completeList), true);
+    t.notEqual(validator(incompleteList), true);
     t.end();
 });
 
-test('Order does not count', function(t) {
-    t.deepEqual(ignoreOrdervalidator(completeList, differentOrder), true);
+test('No string allowed', function(t) {
+    t.notEqual(validator(stringList), false);
     t.end();
-})
+});
+
+test('More than one occurence of a number', function(t){
+    t.equal(validator(multipleOccurance), true);
+    t.end();
+});
