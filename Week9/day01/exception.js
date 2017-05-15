5572692c6d7a4a8164a87d450080f888265d47d0
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-15 11:22:33 +0200

    throws custom error messages, prints str concatenation when parameter printStr is function

---
+++ Week9/day01/exception.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Handle the exceptions in the addString() function. It should check the type of the
+// arguments, throw the right error and write it to the console.
+// It should add the strings too if the arguments are appropriate.
+
+let  addString = function(str1, str2, printStr){
+  let newStr = str1 + str2;
+  if (typeof str1 !== 'string') {
+      throw new Error('"str1" is not a string');
+  } else if (typeof str2 != 'string') {
+      throw new Error('"str2" is not a string');
+  }
+  printStr(newStr);
+}
+
+let printStr = function(str) {
+  console.log(str);
+}
+
+addString(1234, 56789, 'printStr');
