b03781e2ae7c61fc5bebd87c98a361faf7aa6273
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-15 14:48:51 +0200

    tests running, passing

---
+++ Week9/day01/sum/sum.js
@@ -0,0 +1,40 @@
+// Sum
+//
+// Create a sum method in your class which has a list of integers as parameter
+// It should return the sum of the elements in the list
+// Follow these steps:
+// Add a new test case
+// Instantiate your class
+// create a list of integers
+// use the t.equal to test the result of the created sum method
+// Run it
+// Create different tests where you
+// test your method with an empyt list
+// with a list with one element in it
+// with multiple elements in it
+// with a null
+// with a string
+// Run them
+// Fix your code if needed
+'use strict'
+ let sumClass = {
+     sum: function (arr) {
+         let summa = 0;
+         if (Array.isArray(arr) === false) {
+             throw new Error('parameter is not a list')
+         } else {
+             arr.forEach( function(el) {
+                 summa += el;
+             });
+             return summa;
+         }
+     }
+ }
+
+ try {
+     sumClass.sum("bal")
+ } catch (err) {
+     console.log(err.message);
+ }
+
+module.exports = sumClass;
---
+++ Week9/day01/sum/sum_test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let test = require('tape');
+let summing = require('./sum');
+
+
+test('tests for empty list, returns zero', function (t) {
+    t.equal(summing.sum([]), 0);
+    t.end();
+})
+
+test('tests for one element list, returns element', function(t) {
+    t.equal(summing.sum([8]), 8);
+    t.end();
+})
+
+test('returns sum of numbers in list', function (t) {
+    t.equal(summing.sum([2, 3, 4]), 9);
+    t.end();
+})
+
+test('sums one element, zero in list to zero', function(t) {
+    t.equal(summing.sum([0]), 0);
+    t.end();
+})
+
+test('tests for string, throws error message', function(t) {
+    t.throws(function(){
+        throw summing.sum('zsolt')
+    }, Error, 'parameter is not a list');
+    t.end();
+})
