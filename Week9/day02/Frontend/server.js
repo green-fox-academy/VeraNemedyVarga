db5c32880efbcecad425a680171302469f436b82
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-16 15:55:35 +0200

    get method for doubling endpoint written, tests working

--- Week9/day02/Frontend/server.js
+++ Week9/day02/Frontend/server.js
@@ -3,10 +3,39 @@
 var express = require('express');
 let app = express();
 
-app.use(express.static('/assets'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+    app.use('/assets/', express.static('assets'));
 });
 
+app.get('/doubling', function(req, res) {
+    let queryInupt = req.query.input;
+    if (queryInupt === '') {
+        res.send({
+            error: "Please provide an input"
+        })
+    } else {
+        res.send({
+            received: queryInupt,
+            result: queryInupt * 2
+        })
+    }
+});
+
+
 app.listen(8080);
+
+
+
+// Create a GET /doubling endpoint
+// That receives a query parameter: input=15
+// And responds with a json object with the doubled amount of the input:
+// {
+//   "received": 15,
+//   "result": 30
+// }
+// if no input is provided:
+// {
+//   "error": "Please provide an input!"
+// }
