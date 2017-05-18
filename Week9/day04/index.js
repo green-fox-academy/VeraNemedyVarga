ab3b5cb7e752bc412709fabe2bc40e4a634a8a18
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-05-18 18:07:13 +0200

    refactored code to query strings being variables

--- Week9/day04/index.js
+++ Week9/day04/index.js
@@ -1,6 +1,11 @@
 const mysql = require('mysql');
 const express = require('express');
 const app = express();
+let basicTableSQL = ["/books", "SELECT book_mast.book_name, purch_price, aut_name, cate_descrip, pub_name FROM category INNER JOIN purchase ON category.cate_id = purchase.cate_id INNER JOIN  book_mast ON purchase.book_name = book_mast.book_name INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id INNER JOIN author ON publisher.country = author.country"];
+
+let categorySQL = ["/books?category", basicTableSQL + "ORDER BY cate_descrip ASC;"];
+
+
 
 var conn = mysql.createConnection({
     host: 'localhost',
@@ -36,18 +41,22 @@ let tabledrawer = function(rows) {
     return table;
 }
 
-
-app.get('/', function get(req, res) {
-    var result = "";
-    conn.query("SELECT book_mast.book_name, purch_price, aut_name, cate_descrip, pub_name FROM category INNER JOIN purchase ON category.cate_id = purchase.cate_id INNER JOIN  book_mast ON purchase.book_name = book_mast.book_name INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id INNER JOIN author ON publisher.country = author.country;", function(err, rows) {
-        if (err) {
-            console.log("PARA", err);
-        } else {
-            result += tabledrawer(rows);
-        }
-        res.send(result);
+let dataHandler = function(queryInput) {
+    app.get(queryInput[0], function get(req, res) {
+        var result = "";
+        conn.query(queryInput[1], function(err, rows) {
+            if (err) {
+                console.log("PARA", err);
+            } else {
+                result += tabledrawer(rows);
+            }
+            res.send(result);
+        });
     });
-});
+}
+
+dataHandler(basicTableSQL);
+dataHandler(categorySQL);
 
 app.listen(3000, function(){
     console.log("server running");
